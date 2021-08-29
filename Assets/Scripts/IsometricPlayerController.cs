2b2988c2f00f462b0500896ec789b52b946d30b8
Chanatip Kriengkraipetch <mikaecat@gmail.com>
2021-08-29 16:01:47 +0700

    player controller controls more than just movement now

--- Assets/Scripts/IsometricPlayerController.cs
+++ Assets/Scripts/IsometricPlayerController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class IsometricPlayerMovementController : MonoBehaviour
+public class IsometricPlayerController : MonoBehaviour
 {
     public Enums.PlayerCharacterState currentPlayerCharacterState;
 
